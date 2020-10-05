5aed8294aec90e098c108c6d37a1a699e9bb9717
aaronkotch <aaronssikua@gmail.com>
2020-10-06 00:06:45 +0800

    first commit

---
+++ src/Staff/staffDetails.java
@@ -0,0 +1,23 @@
+package Staff;
+
+public class staffDetails {
+
+    private String name;
+    private String email;
+    private String password;
+    private int contactNum;
+
+    public staffDetails(String staffName, String staffEmail, String staffPassword, int staffContactNum) {
+
+        name = staffName;
+        email = staffEmail;
+        password = staffPassword;
+        contactNum = staffContactNum;
+
+    }
+
+
+
+
+    
+}
